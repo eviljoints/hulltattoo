afd9182b1c073e55c6ae64d8267d0887865ceb83
mike laycock <laycock131@gmail.com>
2024-12-18 16:28:10 +0000

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,23 @@
-// src/pages/_app.tsx
+import { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import Head from "next/head";
+import "../styles/globals.css";
 
-import { AppProps } from 'next/app';
-import Layout from '../components/Layout';
-import { ChakraProvider } from '@chakra-ui/react';
-import Head from 'next/head';
-import '../styles/globals.css'; // Ensure you have global styles
+// Force dark mode globally
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark", // Set default theme to dark
+    useSystemColorMode: false, // Ignore system settings
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // If the page component has a 'title' property, use it. Otherwise, default.
-  const pageTitle = (Component as any).title || 'Hull Tattoo Studio';
+  const pageTitle = (Component as any).title || "Hull Tattoo Studio";
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Layout title={pageTitle}>
         <Component {...pageProps} />
       </Layout>
