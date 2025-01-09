5016bfe45883d5df0001d3f2fd1c1e1f5e77cecd
mike laycock <laycock131@gmail.com>
2025-01-09 22:02:14 +0000

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    siteUrl: 'https://hulltattoostudio.com',
-    generateRobotsTxt: true, // Automatically generate robots.txt
-  };
-  
+  siteUrl: 'https://hulltattoostudio.com', // Replace with your website's URL
+  generateRobotsTxt: true, // Automatically generate a robots.txt file
+  generateIndexSitemap: true, // Generates a sitemap index if needed
+  exclude: ['/admin', '/hidden-page'], // Add pages to exclude if necessary
+  changefreq: 'daily', // Default change frequency
+  priority: 0.7, // Default priority
+}; 
