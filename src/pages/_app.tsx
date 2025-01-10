6b0c2ec01a31f10d17557d00b082669b37667c7a
mike laycock <laycock131@gmail.com>
2025-01-10 19:15:01 +0000

    ad test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,40 +1,31 @@
-// src/pages/_app.tsx
-
-import { AppProps } from "next/app";
-import Layout from "../components/Layout";
-import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
-import "../styles/globals.css";
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from '@vercel/analytics/react';
-import Script from 'next/script'; // Import Script from next/script
+import { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import Layout from '../components/Layout';
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
+import '../styles/globals.css';
 
 // Force dark mode globally
 const theme = extendTheme({
   config: {
-    initialColorMode: "dark", // Set default theme to dark
+    initialColorMode: 'dark', // Set default theme to dark
     useSystemColorMode: false, // Ignore system settings
   },
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const pageTitle = (Component as any).title || "Hull Tattoo Studio";
+  useEffect(() => {
+    const adScript = document.createElement('script');
+    adScript.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6959045179650835';
+    adScript.async = true;
+    adScript.crossOrigin = 'anonymous';
+    document.head.appendChild(adScript);
+  }, []);
 
   return (
     <ChakraProvider theme={theme}>
-      {/* Color mode script must be before any other script */}
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-
-      {/* Google AdSense Script */}
-      <Script
-        strategy="afterInteractive" // Load the script after the page becomes interactive
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6959045179650835"
-        crossOrigin="anonymous"
-      />
-
-      <Layout title={pageTitle}>
+      <Layout>
         <Component {...pageProps} />
-        <SpeedInsights />
-        <Analytics />
       </Layout>
     </ChakraProvider>
   );
